9c460b07629661ece04e1505b8007c040f923ac0
mjfalahee <mjfalahee@gmail.com>
2023-06-19 13:22:27 -0700

    reformat to json for projects

--- src/components/projectPage/ProjectPage.tsx
+++ src/components/projectPage/ProjectPage.tsx
@@ -4,9 +4,21 @@ import { useThree } from "@react-three/fiber";
 interface ProjectPageProps {
   visible: boolean;
 }
+interface Project {
+  id: number;
+  title: string | null;
+  shortDescription: string | null;
+  longDescription: string | null;
+  imageURL?: string;
+  githubURL?: string;
+  hostedURL?: string;
+}
 
 const ProjectPage: React.FC<ProjectPageProps> = (props) => {
-  return <div className="project-page-container"></div>;
+  useEffect(() => {
+    // load projects from ../../utils/projects.json
+  });
+  return <iframe className="project-page-container"></iframe>;
 };
 
 export default ProjectPage;
