edc576c2fe09c0e02c39e408c2f889a33dd45dbc
mjfalahee <mjfalahee@gmail.com>
2023-08-08 20:18:14 -0700

    small error with environment variable path

--- src/components/models/LaptopScreen.tsx
+++ src/components/models/LaptopScreen.tsx
@@ -11,38 +11,38 @@ export function Model(props: LaptopScreenProps) {
     untransformed: new THREE.Vector3(1.01, 0.62, -0.1),
   };
   return (
-      <mesh position={[25.6, 3.96, 12.11]} rotation={[0.05, -0.325, 0.02]}>
-        {props.transformBool ? (
-          <Html
-            className="screen-div"
-            transform
-            occlude
-            position={screenPositions.transformed}
-            rotation={[0, Math.PI, 0]}
-          >
-            <div className="wrapper">
-              <iframe
-                className="idle-iframe"
-                src="https://mfalahee.github.io/projectpage/"
-                title="idle-page"
-              />
-            </div>
-          </Html>
-        ) : (
-          <Html
-            className="screen-div no-transform"
-            transform={false}
-            position={screenPositions.untransformed}
-          >
-            <div className="wrapper no-transform">
-              <iframe
-                className="pp-iframe no-transform"
-                src="https://mfalahee.github.io/projectpage/#/pp"
-                title="Project Page"
-              />
-            </div>
-          </Html>
-        )}
-      </mesh>
+    <mesh position={[25.6, 3.96, 12.11]} rotation={[0.05, -0.325, 0.02]}>
+      {props.transformBool ? (
+        <Html
+          className="screen-div"
+          transform
+          occlude
+          position={screenPositions.transformed}
+          rotation={[0, Math.PI, 0]}
+        >
+          <div className="wrapper">
+            <iframe
+              className="idle-iframe"
+              src={`${import.meta.env.VITE_LAPTOP_PATH}#/pp`}
+              title="idle-page"
+            />
+          </div>
+        </Html>
+      ) : (
+        <Html
+          className="screen-div no-transform"
+          transform={false}
+          position={screenPositions.untransformed}
+        >
+          <div className="wrapper no-transform">
+            <iframe
+              className="pp-iframe no-transform"
+              src={`${import.meta.env.VITE_LAPTOP_PATH}#/pp`}
+              title="Project Page"
+            />
+          </div>
+        </Html>
+      )}
+    </mesh>
   );
 }
